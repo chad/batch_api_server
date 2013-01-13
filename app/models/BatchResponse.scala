5ef81945a15eac597eeb7d5fe3ede63d1978bcc2
Chad Fowler <chad@chadfowler.com>
2013-01-12 22:48:46 -0500

    formatting

--- app/models/BatchResponse.scala
+++ app/models/BatchResponse.scala
@@ -6,32 +6,30 @@ import play.api.libs.json._
 case class BatchResult(status:Int, body: String, headers: Map[String,String]) 
 
 object BatchResult {
-  implicit object BatchResultWriter extends Writes[BatchResult] {
-    def writes(batchResult: BatchResult)  = toJson(
-        Map("status" -> toJson(batchResult.status), 
-            "body" -> toJson(batchResult.body), 
-            "headers" -> toJson(batchResult.headers))
-        )
-  }
+	implicit object BatchResultWriter extends Writes[BatchResult] {
+		def writes(batchResult: BatchResult)  = toJson(
+				Map("status" -> toJson(batchResult.status), 
+						"body" -> toJson(batchResult.body), 
+						"headers" -> toJson(batchResult.headers))
+				)
+	}
 }
 
 case class BatchResponse(results: List[BatchResult]) {
-  implicit object BatchResponseWriter extends Writes[BatchResponse] {
-    def writes(batchResponse: BatchResponse) = toJson(Map(
-        "results" -> batchResponse.results.map(toJson(_)).mkString(" ") 
-            ))
-      
-    
-  }
-  def asJson = {
-    toJson(this)
-  }
+	implicit object BatchResponseWriter extends Writes[BatchResponse] {
+		def writes(batchResponse: BatchResponse) = toJson(Map(
+				"results" -> batchResponse.results.map(toJson(_)).mkString(" ") 
+				))
+	}
+	def asJson = {
+			toJson(this)
+	}
 }
 
 
 object BatchResponse {
-  def fromJson(body: JsValue) = {
-    BatchRequest((body \ "ops").as[List[Operation]],
-        (body \ "sequential").as[Boolean])
-  }
+	def fromJson(body: JsValue) = {
+		BatchRequest((body \ "ops").as[List[Operation]],
+				(body \ "sequential").as[Boolean])
+	}
 }
